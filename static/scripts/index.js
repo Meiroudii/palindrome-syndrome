5113f0b752f6f681d00fa5399319e8d5df71f645
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2024-03-15 20:57:18 +0800

    Fix other issues

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,7 +3,29 @@ const systemSettingsPage = document.getElementById("systemSettings");
 const mainPage = document.querySelector(".content-wrapper");
 const navBar = document.querySelector(".header-wrapper");
 const greetings = document.querySelector(".greetings-wrapper");
+const time = document.getElementById("timeCounter");
 
+// TODO: Set timer functions
+let secondsElapse = 0;
+let interval = null;
+
+function setTime() {
+    time.innerHTML = secondsElapse;
+}
+
+function timer() {
+    secondsElapse++;
+    setTime();
+}
+
+// Timer functions
+function startClock() {
+    interval = setInterval(timer, 1000)
+}
+function stopClock() {}
+function resetClock() {}
+
+// UI
 function systemSettingsView() {
     console.log("clicked");
     systemSettingsPage.style.display = "block";
